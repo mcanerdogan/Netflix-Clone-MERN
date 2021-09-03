1aacedbc0db29181d645b1c2808113c926faf9d6
Muratcan Erdoğan <erdoganmcan@gmail.com>
2021-09-03 10:33:29 +0300

    axios package installed. React fetch data with axios and displaying movies from API created

--- client/src/components/listItem/ListItem.jsx
+++ client/src/components/listItem/ListItem.jsx
@@ -4,13 +4,32 @@ import {
   ThumbDownOutlined,
   ThumbUpAltOutlined,
 } from "@material-ui/icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./listItem.scss";
 
-export default function ListItem({ index }) {
+export default function ListItem({ index, item }) {
   const [isHovered, setIsHovered] = useState(false);
-  const trailer =
-    "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    const getMovie = async () => {
+      try {
+        const res = await axios.get("/movies/find/" + item, {
+          headers: {
+            token:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMmY3NTJlNDQ4NzQ1N2FjZGE4ZTk0MiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzMDY1MzA1NywiZXhwIjoxNjMxMDg1MDU3fQ.CDnEWniTe-8sLLXgR9PSD5wAU-R7HrsVDsBgdemvADI",
+          },
+        });
+
+        setMovie(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getMovie();
+  }, [item]);
+
   return (
     <div
       className="listItem"
@@ -18,13 +37,10 @@ export default function ListItem({ index }) {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <img
-        src="https://images.indianexpress.com/2017/12/lord-of-the-rings-759.jpg"
-        alt=""
-      />
+      <img src={movie.img} alt="" />
       {isHovered && (
         <>
-          <video src={trailer} autoPlay={true} loop />
+          <video src={movie.trailer} autoPlay={true} loop />
           <div className="itemInfo">
             <div className="icons">
               <PlayArrow className="icon" />
@@ -33,15 +49,12 @@ export default function ListItem({ index }) {
               <ThumbDownOutlined className="icon" />
             </div>
             <div className="itemInfoTop">
-              <span>1 hour 14 mins</span>
-              <span className="limit">+16</span>
-              <span>1999</span>
+              <span>{movie.duration}</span>
+              <span className="limit">{movie.limit}</span>
+              <span>{movie.year}</span>
             </div>
-            <div className="desc">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-              in ipsa minus rerum est error at sint eligendi quos laborum
-            </div>
-            <div className="genre">Action</div>
+            <div className="desc">{movie.desc}</div>
+            <div className="genre">{movie.genre}</div>
           </div>
         </>
       )}
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,16 +2,42 @@ import Navbar from "../../components/navbar/Navbar";
 import Featured from "../../components/featured/Featured";
 import List from "../../components/list/List";
 import "./home.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const Home = ({type}) => {
+const Home = ({ type }) => {
+  const [lists, setLists] = useState([]);
+  const [genre, setGenre] = useState(null);
+
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+        const res = await axios.get(
+          `lists${type ? "?type=" + type : ""}${
+            genre ? "&genre=" + genre : ""
+          }`,
+          {
+            headers: {
+              token:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMmY3NTJlNDQ4NzQ1N2FjZGE4ZTk0MiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzMDY1MzA1NywiZXhwIjoxNjMxMDg1MDU3fQ.CDnEWniTe-8sLLXgR9PSD5wAU-R7HrsVDsBgdemvADI",
+            },
+          }
+        );
+        setLists(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getRandomLists();
+  }, [type, genre]);
+
   return (
     <div className="home">
       <Navbar />
-      <Featured type={type}/>
-      <List />
-      <List />
-      <List />
-      <List />
+      <Featured type={type} />
+      {lists.map((list) => (
+        <List list={list} key={Math.random()} />
+      ))}
     </div>
   );
 };
